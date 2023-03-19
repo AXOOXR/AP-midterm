db369bb44b4c0bf68546982660fd2d1764145773
Ali Zali <zaliali1100@gmail.com>
2023-03-19 13:15:46 +0330

    MutationRna created

--- gene.cpp
+++ gene.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Gene{
+class Gene {
 private:
     string RNA;
     string DNA;
@@ -13,14 +13,15 @@ private:
 
 public:
     //RNA
-    viod setRna(string r) {RNA = r;} //setter
-    string getRna() {return RNA;} //getter
+    viod setRna(string r) { RNA = r; } //setter
+    string getRna() { return RNA; } //getter
     //DNA
-    viod setDna(string d,string md) { //setter
+    viod setDna(string d, string md) { //setter
         DNA = d;
         MDNA = md;
     }
-    string getDna() {return DNA,MDNA} //getter
+
+    string getDna() { return DNA, MDNA } //getter
 
     //----------------------------------------------------------------------------
 
@@ -28,12 +29,12 @@ public:
     string make_Dna(string r1) {
         int x;
         string y;
-        x =r1.lenght();
-        for (int i=0;i < x;i++) {
-            if (r1[i] == "A"){
-                y +="T";
-            } else if (r1[i] == "T"){
-                y +="A";
+        x = r1.lenght();
+        for (int i = 0; i < x; i++) {
+            if (r1[i] == "A") {
+                y += "T";
+            } else if (r1[i] == "T") {
+                y += "A";
             } else if (r1[i] == "C") {
                 y += "G";
             } else if (r1[i] == "G") {
@@ -44,6 +45,23 @@ public:
 
     }
 
+    //method of Mutation of gene in RNA:
+    string MutationRna(char a, char b, int n) {
+        int i =0;
+        int j =0; //movement step
+        while (i <= n) {
+            if (RNA[j] == a) {
+                RNA[j] = b;
+                i++;
+                j++;
+                if (i==n) { break;}
+            } else {
+                j++;
+            }
+
+        }
+
+    }
 
 
 
